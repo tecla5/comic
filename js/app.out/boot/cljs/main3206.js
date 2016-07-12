84d5caecca7e0542eb82139e789708f28a162097
jcc2303 <cabrera.javier@gmail.com>
2016-07-12 17:25:57 +0200

    publish: Initial dist subtree commit
    generated from commit f26fbab03f0672304b2a9398b544927dd667b500

--- js/app.out/boot/cljs/main3206.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.9.89 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main3206');
-goog.require('cljs.core');
-goog.require('comic.app');
-comic.app.init();
--- js/app.out/boot/cljs/main7791.js
+++ js/app.out/boot/cljs/main7791.js
@@ -1,5 +1,5 @@
 // Compiled by ClojureScript 1.9.89 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main7701');
+goog.provide('boot.cljs.main7791');
 goog.require('cljs.core');
 goog.require('comic.app');
 comic.app.init();
