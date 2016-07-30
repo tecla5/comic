fe55093b1a67acc6b4733fb999b0ed32292bb136
jcc2303 <cabrera.javier@gmail.com>
2016-07-30 18:54:15 +0200

    publish: Merge pull request #8 from tecla5/layout
    generated from commit 9fc71a807a53709127ab66c023230dc6dd666d1b

--- js/app.out/boot/cljs/main7869.js
+++ js/app.out/boot/cljs/main7869.js
@@ -1,5 +1,5 @@
 // Compiled by ClojureScript 1.9.89 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main7791');
+goog.provide('boot.cljs.main7869');
 goog.require('cljs.core');
 goog.require('comic.app');
 comic.app.init();
